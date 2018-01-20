c020e0984f518557728df199e0adecba5742e8a9
Petr Ruzicka <petr.ruzicka@gmail.com>
2018-01-20 16:08:08 +0100

    Whole GitHub page test added

---
+++ link_check.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -eux
+
+# Main URL
+URL="https://github.com/ruzickap/darktable_video_tutorials_list"
+# URL which should not be checked
+IGNORE_URL='assets-cdn\.github\.com'
+
+docker run -it --rm jare/linkchecker --verbose --check-extern --no-robots --recursion-level=1 --ignore-url="$IGNORE_URL" $URL
