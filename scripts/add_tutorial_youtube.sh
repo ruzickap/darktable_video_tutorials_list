9de1df931450a2e79e8c19662f26d0dc54fde461
Petr Ruzicka <petr.ruzicka@gmail.com>
2018-01-21 18:42:30 +0100

    Fix times in README and add_tutorial_youtube script added

---
+++ scripts/add_tutorial_youtube.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -e
+
+TEMP_FILE="/tmp/add_tutorial_youtube"
+
+if [[ $# -lt 2 ]]; then
+  echo
+  echo "Usage:   $0 \"youtube_video_id\" \"time1\" \"time2\" \"time3\" \"timeX\""
+  echo "Example: $0 SKknBy5lX7I 1:10 2:10 3:30"
+  echo
+  exit 1
+fi
+
+YOUTUBE_VIDEO_ID="$1"
+youtube-dl --get-title --get-duration --get-format $YOUTUBE_VIDEO_ID > $TEMP_FILE
+
+YOUTUBE_VIDEO_NAME=`sed -n 1p $TEMP_FILE`
+YOUTUBE_VIDEO_NAME_SCREENSHOT=`echo ${YOUTUBE_VIDEO_NAME} | sed 's/ /_/g;s/#//;s/|/-/'`
+YOUTUBE_VIDEO_LENGTH=`sed -n 2p $TEMP_FILE`
+# Add zero if time looks like "4:34" or "1:23:45" to look like "04:34" or "01:23:45"
+echo $YOUTUBE_VIDEO_LENGTH | grep "^[0-9]:" && YOUTUBE_VIDEO_LENGTH="0${YOUTUBE_VIDEO_LENGTH}"
+YOUTUBE_VIDEO_RESOLUTION=`sed -n 3p $TEMP_FILE | awk '{ print $3 }'`
+rm $TEMP_FILE
+
+cat << EOF
+### [${YOUTUBE_VIDEO_NAME}](https://youtu.be/${YOUTUBE_VIDEO_ID})
+* Author: ????
+* Video Length: ${YOUTUBE_VIDEO_LENGTH}
+* Video Resolution: ${YOUTUBE_VIDEO_RESOLUTION}
+EOF
+
+shift
+while [[ $# -gt 0 ]]; do
+  echo $1 | grep "^[0-9]:" && TIME="0$1" || TIME="$1"
+  echo "![${YOUTUBE_VIDEO_NAME}](screenshots/${YOUTUBE_VIDEO_NAME_SCREENSHOT}-${TIME}.jpg \"${YOUTUBE_VIDEO_NAME} (${TIME})\")"
+  mpv --really-quiet --no-audio --vo=image --vo-image-jpeg-quality=70 --frames=1 --start=${TIME} https://youtu.be/${YOUTUBE_VIDEO_ID}
+  mv 00000001.jpg "${YOUTUBE_VIDEO_NAME_SCREENSHOT}-${TIME}.jpg"
+  shift
+done
+
