7b76051334ff28ef39fc5e1691fda8227a57c969
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-05-10 09:08:05 +0200

    Add shellcheck + change scripts to pass it

--- scripts/download_all_tutorials.sh
+++ scripts/download_all_tutorials.sh
@@ -2,7 +2,8 @@
 
 DESTINATION_DIRECTORY="/var/tmp/test"
 
-for URL in `sed -n 's/^###.*(\(.*\))/\1/p' ../README.md`; do
-  echo "*** $URL"
-  youtube-dl $URL --restrict-filenames -o "$DESTINATION_DIRECTORY/%(uploader)s-%(title)s-%(id)s.%(ext)s"
+sed -n 's/^###.*(\(.*\))/\1/p' ../README.md | while IFS= read -r URL
+do
+  echo "*** ${URL}"
+  youtube-dl "${URL}" --restrict-filenames -o "${DESTINATION_DIRECTORY}/%(uploader)s-%(title)s-%(id)s.%(ext)s"
 done
