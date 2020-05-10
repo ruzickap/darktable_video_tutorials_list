20d3979b84c3231f931a85b12961450469b17857
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-05-10 09:09:31 +0200

    Add shellcheck + change scripts to pass it (#3)

--- scripts/add_tutorial_youtube.sh
+++ scripts/add_tutorial_youtube.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -eu
 
 TEMP_FILE="/tmp/add_tutorial_youtube"
 OUTPUT_FILE="add_tutorial_youtube.md"
@@ -12,18 +12,18 @@ if [[ $# -lt 2 ]]; then
 fi
 
 YOUTUBE_VIDEO_ID="$1"
-echo "*** $YOUTUBE_VIDEO_ID"
-youtube-dl --get-title --get-duration --get-format $YOUTUBE_VIDEO_ID > $TEMP_FILE
+echo "*** ${YOUTUBE_VIDEO_ID}"
+youtube-dl --get-title --get-duration --get-format "${YOUTUBE_VIDEO_ID}" > $TEMP_FILE
 
-YOUTUBE_VIDEO_NAME=`sed -n 1p $TEMP_FILE`
-YOUTUBE_VIDEO_NAME_SCREENSHOT=`echo ${YOUTUBE_VIDEO_NAME} | sed 's/ /_/g;s/#//;s/|/-/'`
-YOUTUBE_VIDEO_LENGTH=`sed -n 2p $TEMP_FILE`
+YOUTUBE_VIDEO_NAME=$(sed -n 1p "${TEMP_FILE}")
+YOUTUBE_VIDEO_NAME_SCREENSHOT=$(echo "${YOUTUBE_VIDEO_NAME}" | sed 's/ /_/g;s/#//;s/|/-/')
+YOUTUBE_VIDEO_LENGTH=$(sed -n 2p "${TEMP_FILE}")
 # Add zero if time looks like "4:34" or "1:23:45" to look like "04:34" or "01:23:45"
-echo $YOUTUBE_VIDEO_LENGTH | grep -q "^[0-9]:" && YOUTUBE_VIDEO_LENGTH="0${YOUTUBE_VIDEO_LENGTH}"
-YOUTUBE_VIDEO_RESOLUTION=`sed -n 3p $TEMP_FILE | awk '{ print $3 }'`
-rm $TEMP_FILE
+echo "${YOUTUBE_VIDEO_LENGTH}" | grep -q "^[0-9]:" && YOUTUBE_VIDEO_LENGTH="0${YOUTUBE_VIDEO_LENGTH}"
+YOUTUBE_VIDEO_RESOLUTION=$(sed -n 3p ${TEMP_FILE} | awk '{ print $3 }')
+rm ${TEMP_FILE}
 
-cat >> $OUTPUT_FILE << EOF
+cat >> ${OUTPUT_FILE} << EOF
 ### [${YOUTUBE_VIDEO_NAME}](https://youtu.be/${YOUTUBE_VIDEO_ID})
 
 * Author: [Shane Milton](https://www.youtube.com/channel/UC9Xdl6CglNwxCZqvwKuE9TA) [http://shanemiltonphotography.com/](http://shanemiltonphotography.com/)
@@ -34,12 +34,12 @@ EOF
 
 shift
 while [[ $# -gt 0 ]]; do
-  echo $1 | grep -q "^[0-9]:" && TIME="0$1" || TIME="$1"
+  echo "$1" | grep -q "^[0-9]:" && TIME="0$1" || TIME="$1"
   echo "***** $TIME"
-  echo "![${YOUTUBE_VIDEO_NAME}](screenshots/${YOUTUBE_VIDEO_NAME_SCREENSHOT}-${TIME}.jpg \"${YOUTUBE_VIDEO_NAME} (${TIME})\")" >> $OUTPUT_FILE
-  mpv --really-quiet --no-audio --vo=image --vo-image-jpeg-quality=70 --frames=1 --start=${TIME} https://youtu.be/${YOUTUBE_VIDEO_ID}
+  echo "![${YOUTUBE_VIDEO_NAME}](screenshots/${YOUTUBE_VIDEO_NAME_SCREENSHOT}-${TIME}.jpg \"${YOUTUBE_VIDEO_NAME} (${TIME})\")" >> ${OUTPUT_FILE}
+  mpv --really-quiet --no-audio --vo=image --vo-image-jpeg-quality=70 --frames=1 --start="${TIME}" "https://youtu.be/${YOUTUBE_VIDEO_ID}"
   mv 00000001.jpg "${YOUTUBE_VIDEO_NAME_SCREENSHOT}-${TIME}.jpg"
   shift
 done
 
-echo >> $OUTPUT_FILE
+echo >> ${OUTPUT_FILE}
--- scripts/link_check.sh
+++ scripts/link_check.sh
@@ -5,4 +5,4 @@ URL="https://github.com/ruzickap/darktable_video_tutorials_list"
 # URL which should not be checked
 IGNORE_URL='assets-cdn\.github\.com'
 
-docker run -it --rm jare/linkchecker --verbose --check-extern --no-robots --recursion-level=1 --threads=4 --ignore-url="$IGNORE_URL" $URL
+docker run -it --rm jare/linkchecker --verbose --check-extern --no-robots --recursion-level=1 --threads=4 --ignore-url="${IGNORE_URL}" ${URL}
