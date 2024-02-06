10d9b30e204c6852505fea70ac5481efd2179a87
Petr Ruzicka <petr.ruzicka@gmail.com>
2024-02-06 10:14:23 +0100

    feat(gha): unify GHA - renovate, megalinter, markdown, and others (#15)

--- scripts/download_all_tutorials.sh
+++ scripts/download_all_tutorials.sh
@@ -2,8 +2,7 @@
 
 DESTINATION_DIRECTORY="/var/tmp/test"
 
-sed -n 's/^###.*(\(.*\))/\1/p' ../README.md | while IFS= read -r URL
-do
+sed -n 's/^###.*(\(.*\))/\1/p' ../README.md | while IFS= read -r URL; do
   echo "*** ${URL}"
   youtube-dl "${URL}" --restrict-filenames -o "${DESTINATION_DIRECTORY}/%(uploader)s-%(title)s-%(id)s.%(ext)s"
 done
